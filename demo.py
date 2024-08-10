6036fbf7d211173fcb4d2a85a0ab5dd3285dd604
Ruoyu Wang <wangr@lilo.stanford.edu>
2024-08-09 20:47:13 -0700

    Updates to default file name

--- demo.py
+++ demo.py
@@ -3,5 +3,5 @@ from helpers.packing_utils import *
 
 if __name__ == "__main__":
     # "spInv_aB", "spInv_Field_Inclination", "spDisambig_Field_Azimuth_Disamb"
-    config_location = './configs/debug.json'
+    config_location = './configs/demo.json'
     SuperSynthIA_predict_packer(config_location)
